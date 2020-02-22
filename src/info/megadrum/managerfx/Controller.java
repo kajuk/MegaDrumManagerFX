dbdf44c2dfe0d91fcee56bb64d627dbfab66d8c2
Dmitri Skachkov <dskachkov@googlemail.com>
2020-02-22 22:29:40 +0000

    Changes for latest Eclipse

--- src/info/megadrum/managerfx/Controller.java
+++ src/info/megadrum/managerfx/Controller.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import org.omg.CORBA.CustomMarshal;
+//import org.omg.CORBA.CustomMarshal;
 
 import info.megadrum.managerfx.data.Config3rd;
 import info.megadrum.managerfx.data.ConfigFull;
